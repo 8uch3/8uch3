d19a9b74fa7057fe0d5721e8e6a181693a361eba
8uch3 <ombeline.rostand@outlook.fr>
2024-11-14 16:21:44 +0100

    Add files via upload

---
+++ lock.js
@@ -0,0 +1,11 @@
+document.getElementById('submitBtn').addEventListener('click', () => {
+    const accessKey = document.getElementById('accessKey').value;
+    const correctKey = '9973'; // Remplacez par la clé que vous voulez utiliser
+
+    if (accessKey === correctKey) {
+        alert('Access Granted!');
+        window.location.href = 'https://www.ecoledirecte.com/login'; // Remplacez par l'URL de la page à accéder
+    } else {
+        alert('Access Denied. Try again.');
+    }
+});
