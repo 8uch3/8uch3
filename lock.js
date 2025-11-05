3d0f8af372cbfcb2fc0ea755934d01abbf980066
8uch3 <ombeline.rostand@outlook.fr>
2025-11-05 18:50:50 +0100

    lock.js

--- lock.js
+++ lock.js
@@ -1,21 +1,13 @@
 document.getElementById('submitBtn').addEventListener('click', () => {
-    const accessKey = document.getElementById('accessKey').value;
-    const correctKey = '9973'; // Remplacez par la clé que vous voulez utiliser
+  const accessKey = document.getElementById('accessKey').value;
 
-    if (accessKey === correctKey) {
-        alert('Access Granted!');
-        window.location.href = 'https://www.ecoledirecte.com/login'; // Remplacez par l'URL de la page à accéder
-    } else {
-        alert('Access Denied. Try again.');
-    }
-
-    const correctKey = 'work'; // Remplacez par la clé que vous voulez utiliser
-
-    if (accessKey === correctKey) {
-        alert('Access Granted!');
-        window.location.href = 'work.html'; // Remplacez par l'URL de la page à accéder
-    } else {
-        alert('Access Denied. Try again.');
-    }
+  if (accessKey === '9973') {
+    alert('Access Granted!');
+    window.location.href = 'https://www.ecoledirecte.com/login';
+  } else if (accessKey === 'work') {
+    alert('Access Granted!');
+    window.location.href = 'work.html';
+  } else {
+    alert('Access Denied. Try again.');
+  }
 });
-
